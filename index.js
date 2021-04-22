1908006ef85067d223b5debf0a057097d0ad9365
yakuza15404976 <82005362+yakuza15404976@users.noreply.github.com>
2021-04-21 21:45:55 -0700

    added some new images

--- index.js
+++ index.js
@@ -3,12 +3,35 @@ function getDateTime() {
     var hour    = now.getHours();
    if(hour < 12){
       document.getElementById("display").innerHTML = "Good Morning";
+      document.getElementById("display").innerHTML = "Good Evening";
+      document.getElementById("morning").src = "images/morning.png";
+      document.getElementById("daynight").style.backgroundImage = "url('https://www.transparenttextures.com/patterns/cubes.png')";
    }
    else if (hour >= 12 && hour <= 17){
      document.getElementById("display").innerHTML = "Good Afternoon";
+     document.getElementById("display").innerHTML = "Good Evening";
+     document.getElementById("morning").src = "images/afternoon.png";
+     document.getElementById("daynight").style.backgroundImage = "url('https://www.transparenttextures.com/patterns/cubes.png')";
    }
-   else if (hour >=17 && hour <= 24){
-         document.getElementById("display").innerHTML = "Good Evening";
+
+   else if (hour >=17 && hour <= 20 ){
+ document.getElementById("display").innerHTML = "Good Evening";
+ document.getElementById("morning").src = "images/evening.png";
+ document.getElementById("daynight").style.backgroundImage = "url('https://www.transparenttextures.com/patterns/cubes.png')";
+
+ // document.getElementById("daynight").style.backgroundPosition = "center";
+ // document.getElementById("daynight").style.backgroundRepeat = "no-repeat";
+ // document.getElementById("daynight").style.borderRadius = "50%";
+// document.getElementById("daynight").style.textAlign ="center";
+ // document.getElementById("daynight").style.backgroundSize = "100%";
+
+
+// image.width = 100%;
+    }
+    else if (hour >= 20 && hour <= 24) {
+      document.getElementById("display").innerHTML = "It's kind of late to be checking on my resume. Good Night.";
+      document.getElementById("morning").src = "images/night.png";
+      document.getElementById("daynight").style.backgroundImage = "url('https://www.transparenttextures.com/patterns/cubes.png')";
     }
 }
 
